a38d44f98771d02a48f53a262f335db49f4aa57c
Ewan Tasker <tasker.ewan@gmail.com>
2025-05-13 09:53:20 +0200

    Adding the configurations for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,62 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, isProduction ? "build" : "dist"),
+      filename: "bundle.js",
+      clean: true,
+      publicPath: "/",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
         {
-          from: "public",
-          to: "",
-          globOptions: {
-            ignore: ["**/index.html"],
-          },
+          test: /\.(ts|tsx)$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          exclude: /node_modules/,
         },
       ],
-    }),
-  ],
-  devServer: {
-    static: "./dist",
-    port: 4400,
-    open: true,
-    historyApiFallback: true,
-  },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "public",
+            to: "",
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+          },
+        ],
+      }),
+    ],
+    devServer: {
+      static: "./dist",
+      port: 4400,
+      open: true,
+      historyApiFallback: true,
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+  };
 };
